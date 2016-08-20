1e454b31c59a10d0a062973f31a1bcec1615c8d4
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-08-20 17:09:29 -0500

    Moving over to syn-style metadata

--- depman/__init__.py
+++ depman/__init__.py
@@ -3,3 +3,7 @@ from .operation import *
 from .dependency import *
 from .apt import *
 from .pip import *
+
+from syn.base_utils import harvest_metadata, delete
+with delete(harvest_metadata, delete):
+    harvest_metadata('metadata.yml')
--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     long_description = read('README.rst'),
     url = 'https://github.com/mbodenhamer/depman',
     packages = find_packages(),
+    include_package_data = True,
     install_requires = requirements('requirements.in'),
     entry_points = {
         'console_scripts': [
